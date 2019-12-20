f1b13a06d5208888a28ec24b49d0f53991224bbd
Barna Burom <barna@codingsans.com>
2019-12-20 13:12:13 +0100

    feat(Player): isStraight

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,3 +1,5 @@
+export const rankOrder = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
 export interface HoleCard {
   rank: '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K' | 'A';
   suit: 'hearts' | 'spades' | 'clubs' | 'diamonds';
