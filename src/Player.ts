b8c29a3f45d302297b98e0814502334bcb0484a1
Laszlo Csele <laszlo.csele90@gmail.com>
2019-12-20 11:26:14 +0100

    4

--- src/Player.ts
+++ src/Player.ts
@@ -1,4 +1,12 @@
 import { GameState } from "./interfaces";
+import * as _ from "lodash";
+
+function hasEqualRank(rankGroups: { [rank: string]: string[] }, num: number) {
+  return _.some(
+    Object.keys(rankGroups),
+    rank => rankGroups[rank].length === num
+  );
+}
 
 export class Player {
   public betRequest(
@@ -8,13 +16,25 @@ export class Player {
     const me = gameState.players[gameState.in_action];
     const cards = me.hole_cards;
     const pair = cards[0].rank === cards[1].rank;
-
-    if (!pair) {
-      betCallback(0);
+    const ranks = [
+      ...cards.map(c => c.rank),
+      ...gameState.community_cards.map(c => c.rank)
+    ];
+    const rankGroups = _.groupBy(ranks, r => r);
+    const allIn = me.stack - me.bet;
+    if (hasEqualRank(rankGroups, 4)) {
+      betCallback(allIn);
     }
-    const amount =
-      gameState.current_buy_in - gameState.players[gameState.in_action].bet;
-    betCallback(amount);
+
+    if (hasEqualRank(rankGroups, 3)) {
+      betCallback(200);
+    }
+
+    if (hasEqualRank(rankGroups, 2)) {
+      betCallback(100);
+    }
+
+    betCallback(0);
   }
 
   public showdown(gameState: any): void {}
