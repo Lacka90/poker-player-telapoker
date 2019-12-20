a486a3a55d59492b75b9e42efd806879f7f6da8a
Laszlo Csele <laszlo.csele90@gmail.com>
2019-12-20 11:01:21 +0100

    1

--- src/Player.ts
+++ src/Player.ts
@@ -1,11 +1,11 @@
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    betCallback(0);
+    const amount =
+      gameState.current_buy_in - gameState.players[gameState.in_action].bet;
+    betCallback(amount);
   }
 
-  public showdown(gameState: any): void {
-
-  }
-};
+  public showdown(gameState: any): void {}
+}
 
 export default Player;
